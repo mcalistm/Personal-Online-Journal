5952e549fd3c47ef633deaf08db68273610cb4ae
Maggie McAlister <mcalistm@tcd.ie>
2021-02-08 13:27:00 +0000

    Updated imports and app.configs in app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
 from flask import Flask
+from flask_pymongo import flask_pymongof
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route("/")
-def hello():
-    return "Hello World ... again!"
+
+@app.route('/')
+def home():
+    """
+    Function loads the homepage
+    """
+    return render_template('pages/home.html')
 
 
 if __name__ == "__main__":
